62d7c64ec04436121680298717558a86f8f1da20
Tom Andersson <tomand@bas.ac.uk>
2023-05-31 10:13:34 +0100

    `setup.py` template copied from `neuralprocesses`, allowing `pip install -e .`

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "numpy>=1.16",
+    "backends>=1.4.31",
+    "backends-matrix>=1.2.10",
+    "plum-dispatch>=2",
+    "wbml>=0.3.18",
+]
+
+setup(
+    packages=find_packages(exclude=["docs"]),
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+)
