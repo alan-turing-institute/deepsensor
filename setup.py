0a5d43f78b271d5c035afd56a37988e0e80a2f24
Tom Andersson <tomand@bas.ac.uk>
2023-06-11 21:55:07 +0100

    Try to set up autoversioning in pip upload based on GitHub version

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup
+import subprocess
+import os
+
+deepsensor_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+assert "-" not in deepsensor_version
+assert "." in deepsensor_version
+
+assert os.path.isfile("deepsensor/version.py")
+with open("deepsensor/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % deepsensor_version)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        package_data={"deepsensor": ["VERSION"]},
+        include_package_data=True,
+    )
