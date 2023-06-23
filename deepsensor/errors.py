3b2f21fe7e1e4fd34e5bca86c2baed8a0e03ea67
Tom Andersson <tomand@bas.ac.uk>
2023-06-23 14:57:38 +0100

    DeepSensor-specific exceptions

---
+++ deepsensor/errors.py
@@ -0,0 +1,10 @@
+class TaskSetIndexError(Exception):
+    """Raised when the task context/target set index is out of range."""
+    def __init__(self, index, set_length, context_or_target):
+        super().__init__(f"{context_or_target} set index {index} is out of range for task with "
+                         f"{set_length} {context_or_target} sets.")
+
+
+class GriddedDataError(Exception):
+    """Raised during invalid operation with gridded data"""
+    pass
