01c29037ad3e6e3107453c6093b8e6c6c3bde3fc
Tom Andersson <tomand@bas.ac.uk>
2023-08-21 19:21:12 +0100

    Approximate resolution of pointwise pandas data for inferring model ppu; Use max of data ppus over context/target data for inferring ppu.

--- deepsensor/model/defaults.py
+++ deepsensor/model/defaults.py
@@ -1,41 +1,106 @@
 from deepsensor.data.loader import TaskLoader
-from typing import Union
-from scipy.spatial.distance import pdist
+
+import scipy
 
 import numpy as np
 import pandas as pd
 import xarray as xr
-import warnings
+
+from typing import Union
+
+
+def compute_xarray_data_resolution(ds: Union[xr.DataArray, xr.Dataset]) -> float:
+    """Computes the resolution of an xarray object with coordinates x1 and x2
+
+    Args:
+        ds (Union[xr.DataArray, xr.Dataset]): Xarray object with coordinates x1 and x2.
+
+    Returns:
+        data_resolution (float): Resolution of the data (in spatial units).
+    """
+    x1_res = np.abs(np.mean(np.diff(ds["x1"])))
+    x2_res = np.abs(np.mean(np.diff(ds["x2"])))
+    data_resolution = np.mean([x1_res, x2_res])
+    return data_resolution
+
+
+def compute_pandas_data_resolution(
+    df: Union[pd.DataFrame, pd.Series], n_times_samples: int = 1000, percentile: int = 5
+) -> float:
+    """Approximates the resolution of non-gridded pandas data with indexes time, x1, and x2.
+
+    The resolution is approximated from a random selection of dates by, for each date:
+     - computing the shortest distance to another observation for each observation
+     - taking the 5th percentile (by default) to avoid outliers from the shortest distances
+    Then, the mean of these resolutions is returned.
+
+    Args:
+        df (Union[pd.DataFrame, pd.Series]): Dataframe or series with indexes time, x1, and x2.
+        n_times_samples (int, optional): Number of dates to sample. Defaults to 1000. If "all",
+            all dates are used.
+        percentile (int, optional): Percentile of pairwise distances for computing the resolution
+            of each time slice. Defaults to 5.
+
+    Returns:
+        data_resolution (float): Resolution of the data (in spatial units).
+    """
+    dates = df.index.get_level_values("time").unique()
+
+    if len(dates) > n_times_samples:
+        rng = np.random.default_rng(42)
+        dates = rng.choice(dates, size=n_times_samples, replace=False)
+
+    data_resolutions = []
+    df = df.reset_index().set_index("time")
+    for time in dates:
+        df_t = df.loc[time]
+        X = df_t[["x1", "x2"]].values  # (N, 2) array of coordinates
+        X_unique = np.unique(X, axis=0)  # (N_unique, 2) array of unique coordinates
+
+        pairwise_distances = scipy.spatial.distance.cdist(X_unique, X_unique)
+        percentile_distances_without_self = np.ma.masked_equal(pairwise_distances, 0)
+
+        # Compute the closest distance from each station to each other station
+        closest_distances = np.min(percentile_distances_without_self, axis=1)
+        data_resolution = np.percentile(closest_distances, percentile)
+
+        data_resolutions.append(data_resolution)
+
+    return np.mean(data_resolutions)
+
 
 def gen_ppu(task_loader: TaskLoader) -> int:
     """Computes data-informed settings for the model's internal discretisation density (ppu, points per unit)
 
-    For gridded data, the ppu should be as small as possible given the data's resolution. The value
-    chosen is 1.2 * data_ppu, where data_ppu is the number of data points per unit.
+    Loops over all context and target variables in the `TaskLoader` and computes the data resolution
+    for each. The model ppu is then set to the maximum data ppu.
+    - Xarray: data resolution is computed from the mean of the x1 and x2 coordinate resolutions
+    - Pandas: data resolution is approximated using the distances between observations
 
-    For off-grid station data, the ppu should be as large as possible so that the model can
-    represent the data as accurately as possible. The value chosen is 300.
+    Args:
+        task_loader (TaskLoader): TaskLoader object containing context and target sets.
+
+    Returns:
+        model_ppu (int): Model ppu (points per unit), i.e. the number of points per unit of input space.
     """
-    max_ppu = 0
+    # List of data resolutions for each context/target variable (in points-per-unit)
+    data_ppus = []
     for var in [*task_loader.context, *task_loader.target]:
         if isinstance(var, (xr.DataArray, xr.Dataset)):
-            # Gridded variable: use data ppu
-            x1_res = np.abs(np.mean(np.diff(var["x1"])))
-            x2_res = np.abs(np.mean(np.diff(var["x2"])))
-            data_ppu = 1 / np.mean([x1_res, x2_res])
-            max_ppu = max(max_ppu, data_ppu * 1.2)  # Add 20% margin
+            # Gridded variable: use data resolution
+            data_resolution = compute_xarray_data_resolution(var)
         elif isinstance(var, (pd.DataFrame, pd.Series)):
-            # Point-based variable: calculate ppu based on pairwise distances
-            coordinates = var[['x1', 'x2']].values if isinstance(var, pd.DataFrame) else var.values  # Adjust based on data format
-            distances = pdist(coordinates)
-            quantile_distance = np.percentile(distances, 5)  # 5th percentile distance
-            point_ppu = 1 / quantile_distance
-            max_ppu = min(max_ppu, point_ppu, 500)  # Introducing an upper bound
-            warnings.warn(f"Point-based data detected. ppu set to {max_ppu}. Consider choosing a suitable value based on the data.")
+            # Point-based variable: calculate ppu based on pairwise distances between observations
+            data_resolution = compute_pandas_data_resolution(
+                var, n_times_samples=1000, percentile=5
+            )
         else:
             raise ValueError(f"Unknown context input type: {type(var)}")
+        data_ppu = int(1 / data_resolution)
+        data_ppus.append(data_ppu)
 
-    return int(max_ppu)
+    model_ppu = int(max(data_ppus))
+    return model_ppu
 
 
 def gen_decoder_scale(model_ppu: int) -> float:
