09befb3175d9db6660a52a22010cd29befc063e1
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-07-19 16:48:39 +0100

    test parameter handling and sizes in patchwise prediction

--- tests/test_model.py
+++ tests/test_model.py
@@ -541,13 +541,58 @@ class TestModel(unittest.TestCase):
 
         model = ConvNP(self.dp, tl)
 
-        model.predict_patch(
-            tasks=task,
-            X_t=self.da,
-            data_processor=self.dp,
-            stride=stride_size,
+        pred = model.predict_patch(
+                tasks=task,
+                X_t=self.da,
+                data_processor=self.dp,
+                stride=stride_size,
+                patch_size=patch_size,
+            )
+
+        # gridded predictions
+        assert [isinstance(ds, xr.Dataset) for ds in pred.values()]
+        for var_ID in pred:
+            assert_shape(
+                pred[var_ID]["mean"],
+                (1, self.da.x1.size, self.da.x2.size),
+            )
+            assert_shape(
+                pred[var_ID]["std"],
+                (1, self.da.x1.size, self.da.x2.size),
+            )
+            assert(
+                self.da.x1.size == pred[var_ID].x1.size
+            )
+            assert(
+                self.da.x2.size == pred[var_ID].x2.size
+            )
+
+
+    @parameterized.expand([(0.5, 0.6)])
+    def test_patchwise_prediction_parameter_handling(self, patch_size, stride_size):
+        """Test that correct errors and warnings are raised by ``.predict_patch``."""
+
+        tl = TaskLoader(context=self.da, target=self.da)
+
+        task = tl(
+            "2020-01-01",
+            context_sampling="all",
+            target_sampling="all",
+            patch_strategy="sliding",
             patch_size=patch_size,
+            stride=stride_size,
         )
+
+        model = ConvNP(self.dp, tl)
+
+        with self.assertRaises(ValueError):
+            model.predict_patch(
+                tasks=task,
+                X_t=self.da,
+                data_processor=self.dp,
+                stride=stride_size,
+                patch_size=patch_size,
+            )
 
     def test_saving_and_loading(self):
         """Test saving and loading of model"""
